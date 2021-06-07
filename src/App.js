d759c97b16574fea2a16d10ecbf706622e50ff79
Jens <jens.clayson@gmail.com>
2021-06-06 19:52:46 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Container } from "@material-ui/core";
 
 function App() {
+  const [lights, setLights] = useState([]);
+
+  // api token
+  const username = "";
+
+  //hub ip address
+  const ip = "192.168.1.1";
+  const url = `http://${ip}/api/${username}`;
+
+  useEffect(() => {
+    axios
+      .get(`${url}/lights`)
+      .then((res) => {
+        const foundLights = Object.keys(res.data).map((key) => {
+          let light = res.data[key];
+          light.id = key;
+          return light;
+        });
+        setLights(foundLights);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hue Controller</h1>
     </div>
   );
 }
---
+++ src/components/Light.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Light = () => {
+  return <div>Light Component</div>;
+};
+
+export default Light;
