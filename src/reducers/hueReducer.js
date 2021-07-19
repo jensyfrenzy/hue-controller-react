f6bcab6c1d3587131c00cd4237441259ad90a6af
Jens <jens.clayson@gmail.com>
2021-07-19 16:24:04 -0500

    Implemented redux and initial room behavior

---
+++ src/reducers/hueReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  rooms: [],
+};
+
+const hueReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_ROOMS":
+      return { ...state, rooms: action.payload.rooms };
+    case "SET_ROOM_STATE":
+      return { ...state, rooms: [...rooms] };
+    default:
+      return { ...state };
+  }
+};
+
+export default hueReducer;
